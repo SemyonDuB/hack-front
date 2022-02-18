0f75cdd0f7fec920a3929ee8986130a679b78101
Simon Dubskikh <dubskikhsemyon@gmail.com>
2022-02-18 14:42:35 +0500

    First steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {DatePicker} from 'antd';
+import {Avatar} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import {Row, Col} from 'antd';
+import {Layout} from 'antd';
+import {Skeleton} from 'antd';
+import { Typography, Divider } from 'antd';
+import {Button} from "antd";
+
+const { Title, Paragraph, Text, Link } = Typography;
+const {Header, Footer, Sider, Content} = Layout;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<>
+        <Layout>
+            <Header><Title level={3} >Header</Title></Header>
+            <Content style={{padding: '50px 50px'}}>
+                <Row justify="center">
+                    <h1>Имя Фамилия Отчество</h1>
+                </Row>
+                <Row justify="center">
+                    <h3>Статус</h3>
+                </Row>
+                <Row justify="center">
+                    <Col span={6}>
+                        <Avatar size={{lg: 150}} icon={<UserOutlined/>}/>
+                    </Col>
+                    <Col span={6}>
+                        <Skeleton/>
+                    </Col>
+                </Row>
+                <Button href="https://google.com">Войти с гугл</Button>
+            </Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    </>);
 }
 
 export default App;
